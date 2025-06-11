d47fbdcbe470b82c5eba215a5f84e2d43d735f51
TThompson15 <toroktamas09@gmail.com>
2025-06-11 19:12:00 +0200

    Added in new function: blocks

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,22 @@ import java.awt.event.InputEvent;
 import java.util.Scanner;
 
 /**
- * The class is responsible for a mouse auto clicker, which does a left mouse button click at the user defined time interval *
- * */
+ * The class is responsible for a mouse auto-clicker, which performs a left mouse button click.
+ *
+ * The user can provide:
+ *      - The time interval between two clicks
+ *      - Whether the program should run indefinitely or stop after a certain number of clicks
+ *
+ * If the program is perpetual ('y'):
+ *      - The code will run in an infinite loop
+ *      - Clicks will occur at the interval specified by the user
+ *
+ * If the program is not perpetual ('n'):
+ *      - The user can specify the total number of clicks
+ *      - The user can specify how many clicks should occur in each clicking block (section)
+ *      - The user can specify the wait time between each block of clicks
+ *
+ */
 public class Main {
 
     /**
@@ -18,8 +32,18 @@ public class Main {
     private static int milliseconds = 0;
 
     /**
+     * The integer which represents the clicks in each clicking block. Only used when the clicker is NOT infinite
+     */
+    private static int clicksPerBlock = 0;
+
+    /**
+     * The integer which represents the time between each clicking block. Only used when the clicker is NOT infinite
+     */
+    private static int waitTimeBetweenBlocks = 0;
+
+    /**
      * The integer which represents the number of clicks.
-     * If it is 0 (which is only available if the user gives 0 as its clicks), the program will run perpetually.
+     * If it is 0 (which is ONLY available if the user decides the clicks are perpetual), the program will run in an infinite loop.
      * */
     private static int clicks = -1;
 
@@ -29,8 +53,14 @@ public class Main {
      * @param args: program arguments, not implemented in this code
      * */
     public static void main(String[] args) {
+
+        // The time(ms) given by the user will directly go into the --milliseconds-- variable
         millisecondIntervals();
+
+        // The number of clicks given by the user will directly go into the --clicks-- variable
         numberOfClicks();
+
+        // Run the program
         runClicker();
     }
 
@@ -45,6 +75,8 @@ public class Main {
 
             if (input.equalsIgnoreCase("y")) {
                 clicks = 0;
+
+                // If the clicking is perpetual, we save the clicks as 0, and we break out.
                 return;
             } else if (input.equalsIgnoreCase("n")) {
                 System.out.println("Alright, give me the number of total clicks:");
@@ -53,15 +85,18 @@ public class Main {
                         String clickInput = sc.nextLine();
                         clicks = Integer.parseInt(clickInput);
                         if (clicks > 0) {
+                            // Calling the next method, because we got the clicks provided by the user, and it got saved.
+                            // The method is only called when the clicks arent infinite (infinite here is clicks=0)
+                            waitTimeBetweenClicks();
                             return;
                         }
-                        System.out.println("Please enter a positive integer: ");
+                        System.out.println("Please enter a positive integer!");
                     } catch (Exception e) {
-                        System.out.println("Sorry, I couldn't read that! Please enter a valid number: ");
+                        System.out.println("Sorry, I couldn't read that! Please enter a valid number! ");
                     }
                 }
             } else {
-                System.out.println("Try again!");
+                System.out.println("Please type y(perpetual) or n(not perpetual)!");
             }
         }
     }
@@ -71,8 +106,7 @@ public class Main {
      * The method responsible for getting the time between clicks from the user through the terminal.
      * */
     private static void millisecondIntervals() {
-        System.out.println("""
-                
+        System.out.println("""                
                 Hi!
                 This is my autoclicker, please provide me the intervals between the clicks in milliseconds
                 (1000ms = 1s):""");
@@ -82,11 +116,50 @@ public class Main {
                 String input = sc.nextLine();
                 milliseconds = Integer.parseInt(input);
                 if (milliseconds > 0) {
+                    // If we get a good input, we'll break out of the method. We got what we wanted, milliseconds have been saved
                     break;
                 }
-                System.out.println("Please enter a positive integer.");
+                System.out.println("Please enter a positive integer!");
             } catch (Exception e) {
-                System.out.println("Error! Please provide me the intervals between milliseconds: ");
+                System.out.println("Sorry, I couldn't read that! Please enter a valid number: ");
+            }
+        }
+    }
+
+
+    /**
+     * The method responsible for getting the waiting time between clicking blocks, got by the user through terminal.
+     * */
+    private static void waitTimeBetweenClicks() {
+        System.out.println("Awesome! Now configure block settings (each block consists of X clicks, and Y time between blocks:");
+
+        while (true) {
+            try {
+                System.out.println("How many clicks should be in one block (X)?");
+                clicksPerBlock = Integer.parseInt(sc.nextLine());
+                if (clicksPerBlock > 0) {
+                    // We got what we wanted, no more questions here, going to waitTimeBetweenBlocks
+                    break;
+                } else {
+                    System.out.println("Please enter a positive integer!");
+                }
+            } catch (Exception e) {
+                System.out.println("Sorry, I couldn't read that! Please enter a valid number! ");
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.println("How long should be the break between blocks (Y)? (in milliseconds)");
+                waitTimeBetweenBlocks = Integer.parseInt(sc.nextLine());
+                if (waitTimeBetweenBlocks > 0) {
+                    // We are finished, breaking out of method, and moving to runClicker()
+                    break;
+                } else {
+                    System.out.println("Please enter a positive integer!");
+                }
+            } catch (Exception e) {
+                System.out.println("Sorry, I couldn't read that! Please enter a valid number! ");
             }
         }
     }
@@ -106,21 +179,34 @@ public class Main {
 
             Robot robot = new Robot();
 
+            // If the clicking is infinite, there are no blocks, just clicks, infinitely
             if (clicks == 0) {
                 while (true) {
                     performClick(robot);
                     Thread.sleep(milliseconds);
                 }
             } else {
-                for (int i = 0; i < clicks; i++) {
-                    performClick(robot);
-                    Thread.sleep(milliseconds);
+                // Otherwise, there are blocks, which sleep from one another
+
+                int completedClicks = 0;
+                while (completedClicks < clicks) {
+                    int clicksThisBlock = Math.min(clicksPerBlock, clicks - completedClicks);
+                    for (int i = 0; i < clicksThisBlock; i++) {
+                        performClick(robot);
+                        Thread.sleep(milliseconds);
+                        completedClicks++;
+                    }
+
+                    if (completedClicks < clicks) {
+                        System.out.println("Block done. Waiting " + waitTimeBetweenBlocks + " ms before next block...");
+                        Thread.sleep(waitTimeBetweenBlocks);
+                    }
                 }
             }
 
             System.out.println("Finished clicking!");
         } catch (Exception e) {
-            System.out.println("Something went wrong! Shutting down");
+            System.out.println("Something went wrong! Shutting down.");
         }
     }
 
